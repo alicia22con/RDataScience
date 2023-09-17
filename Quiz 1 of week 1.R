56f43d47686e69e4271fcadcc890706bbe250eef
Alicia M Conner <145229754+alicia22con@users.noreply.github.com>
2023-09-17 07:44:34 -0700

    testing

---
+++ Quiz 1 of week 1.R
@@ -0,0 +1,42 @@
+# fread url requires curl package on mac 
+# install.packages("curl")
+
+library(data.table)
+housing <- data.table::fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv")
+
+# VAL attribute says how much property is worth, .N is the number of rows
+# VAL == 24 means more than $1,000,000
+housing[VAL == 24, .N]
+
+# Answer: 
+# 53
+
+sum(dat$Zip*dat$Ext,na.rm=T)
+
+fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileUrl, destfile = paste0(getwd(), '/getdata%2Fdata%2FDATA.gov_NGAP.xlsx'), method = "curl")
+
+dat <- xlsx::read.xlsx(file = "getdata%2Fdata%2FDATA.gov_NGAP.xlsx", sheetIndex = 1, rowIndex = 18:23, colIndex = 7:15)
+sum(dat$Zip*dat$Ext,na.rm=T)
+
+# Answer:
+# 36534720
+
+# install.packages("XML")
+library("XML")
+fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+doc <- XML::xmlTreeParse(sub("s", "", fileURL), useInternal = TRUE)
+rootNode <- XML::xmlRoot(doc)
+
+zipcodes <- XML::xpathSApply(rootNode, "//zipcode", XML::xmlValue)
+xmlZipcodeDT <- data.table::data.table(zipcode = zipcodes)
+xmlZipcodeDT[zipcode == "21231", .N]
+
+# Answer: 
+# 127
+
+DT <- data.table::fread("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv")
+
+# Answer (fastest):
+system.time(DT[,mean(pwgtp15),by=SEX])
+
